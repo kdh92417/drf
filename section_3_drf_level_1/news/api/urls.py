ccb231c8a3396e0f74e326297e978d08e0f5d3ca
Adam <kdh92417@gmail.com>
2021-07-09 17:57:02 +0900

    [DRF] Add - Funtional Based View & Class Based View

---
+++ section_3_drf_level_1/news/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls    import path
+from news.api.views import (
+    ArticleCreateListAPIView,
+    ArticleDetailAPIView
+)
+
+# from news.api.views import (
+#     article_list_create_api_view,
+#     article_detail_api_view
+# )
+
+urlpatterns = [
+    path('articles/', ArticleCreateListAPIView.as_view(), name='article-list'),
+    path('articles/<int:pk>/', ArticleDetailAPIView.as_view(), name='article-detail'),
+]
+
+# urlpatterns = [
+#     path('articles/', article_list_create_api_view, name='article-list'),
+#     path('articles/<int:pk>/', article_detail_api_view, name='article-detail'),
+# ]
