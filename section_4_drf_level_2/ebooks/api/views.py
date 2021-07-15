6391df100af74b276e59760e5fd2b96edeb5c361
Adam <kdh92417@gmail.com>
2021-07-15 17:07:02 +0900

    [DRF] Study concrete API View

---
+++ section_4_drf_level_2/ebooks/api/views.py
@@ -0,0 +1,54 @@
+from rest_framework import (
+    generics,
+    mixins
+)
+
+
+from ebooks.models import (
+    Ebook,
+    Review
+)
+
+from ebooks.api.serializers import (
+    EbookSerializer,
+    ReviewSerializer
+)
+
+
+class EbookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        ebook_pk = self.kwargs.get('ebook_pk')
+        ebook = generics.get_object_or_404(Ebook, pk=ebook_pk)
+        serializer.save(ebook=ebook)
+
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+# class EbookListCreateAPIView(mixins.ListModelMixin,
+#                              mixins.CreateModelMixin,
+#                              generics.GenericAPIView):
+
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
